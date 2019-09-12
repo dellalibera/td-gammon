5c79c6a6d27ab08828b754c6e5de2d704eae3b57
Alessio Della Libera <43420907+dellalibera@users.noreply.github.com>
2019-09-12 16:47:42 +0200

    Replaced assert with better error message

--- td_gammon/utils.py
+++ td_gammon/utils.py
@@ -1,6 +1,6 @@
 import os
 import gym
-
+import sys
 from agents import TDAgent, HumanAgent, TDAgentGNU, RandomAgent, evaluate_agents
 from gnubg.gnubg_backgammon import GnubgInterface, GnubgEnv, evaluate_vs_gnubg
 from gym_backgammon.envs.backgammon import WHITE, BLACK
@@ -18,6 +18,15 @@ def write_file(path, **kwargs):
             file.write("{}={}\n".format(key, value))
             print("{}={}".format(key, value))
         print()
+
+
+def path_exists(path):
+    if os.path.exists(path):
+        return True
+    else:
+        print("The path {} doesn't exists".format(path))
+        sys.exit()
+
 
 
 # ==================================== TRAINING PARAMETERS ===================================
@@ -46,12 +55,12 @@ def args_train(args):
         optimizer = True
         env = gym.make('gym_backgammon:backgammon-pixel-v0')
 
-    if args.model:
-        assert os.path.exists(args.model), print("The path {} doesn't exists".format(args.model))
+    if args.model and path_exists(args.model):
+        # assert os.path.exists(args.model), print("The path {} doesn't exists".format(args.model))
         net.load(checkpoint_path=args.model, optimizer=optimizer, eligibility_traces=eligibility)
 
-    if args.save_path:
-        assert os.path.exists(args.save_path), print("The path {} doesn't exists".format(args.save_path))
+    if args.save_path and path_exists(args.save_path):
+        # assert os.path.exists(args.save_path), print("The path {} doesn't exists".format(args.save_path))
         save_path = args.save_path
 
         write_file(
@@ -65,20 +74,21 @@ def args_train(args):
 
 # ==================================== WEB GUI PARAMETERS ====================================
 def args_gui(args):
-    assert os.path.exists(args.model), print("The path {} doesn't exists".format(args.model))
+    if path_exists(args.model):
+        # assert os.path.exists(args.model), print("The path {} doesn't exists".format(args.model))
 
-    if args.type == 'nn':
-        net = TDGammon(hidden_units=args.hidden_units, lr=0.1, lamda=None, init_weights=False)
-        env = gym.make('gym_backgammon:backgammon-v0')
-    else:
-        net = TDGammonCNN(lr=0.0001)
-        env = gym.make('gym_backgammon:backgammon-pixel-v0')
+        if args.type == 'nn':
+            net = TDGammon(hidden_units=args.hidden_units, lr=0.1, lamda=None, init_weights=False)
+            env = gym.make('gym_backgammon:backgammon-v0')
+        else:
+            net = TDGammonCNN(lr=0.0001)
+            env = gym.make('gym_backgammon:backgammon-pixel-v0')
 
-    net.load(checkpoint_path=args.model, optimizer=None, eligibility_traces=False)
+        net.load(checkpoint_path=args.model, optimizer=None, eligibility_traces=False)
 
-    agents = {BLACK: TDAgent(BLACK, net=net), WHITE: HumanAgent(WHITE)}
-    gui = GUI(env=env, host=args.host, port=args.port, agents=agents)
-    gui.run()
+        agents = {BLACK: TDAgent(BLACK, net=net), WHITE: HumanAgent(WHITE)}
+        gui = GUI(env=env, host=args.host, port=args.port, agents=agents)
+        gui.run()
 
 
 # =================================== EVALUATE PARAMETERS ====================================
@@ -90,24 +100,25 @@ def args_evaluate(args):
     hidden_units_agent1 = args.hidden_units_agent1
     n_episodes = args.episodes
 
-    assert os.path.exists(model_agent0), print("The path {} doesn't exists".format(model_agent0))
-    assert os.path.exists(model_agent1), print("The path {} doesn't exists".format(model_agent1))
+    if path_exists(model_agent0) and path_exists(model_agent1):
+        # assert os.path.exists(model_agent0), print("The path {} doesn't exists".format(model_agent0))
+        # assert os.path.exists(model_agent1), print("The path {} doesn't exists".format(model_agent1))
 
-    if model_type == 'nn':
-        net0 = TDGammon(hidden_units=hidden_units_agent0, lr=0.1, lamda=None, init_weights=False)
-        net1 = TDGammon(hidden_units=hidden_units_agent1, lr=0.1, lamda=None, init_weights=False)
-        env = gym.make('gym_backgammon:backgammon-v0')
-    else:
-        net0 = TDGammonCNN(lr=0.0001)
-        net1 = TDGammonCNN(lr=0.0001)
-        env = gym.make('gym_backgammon:backgammon-pixel-v0')
+        if model_type == 'nn':
+            net0 = TDGammon(hidden_units=hidden_units_agent0, lr=0.1, lamda=None, init_weights=False)
+            net1 = TDGammon(hidden_units=hidden_units_agent1, lr=0.1, lamda=None, init_weights=False)
+            env = gym.make('gym_backgammon:backgammon-v0')
+        else:
+            net0 = TDGammonCNN(lr=0.0001)
+            net1 = TDGammonCNN(lr=0.0001)
+            env = gym.make('gym_backgammon:backgammon-pixel-v0')
 
-    net0.load(checkpoint_path=model_agent0, optimizer=None, eligibility_traces=False)
-    net1.load(checkpoint_path=model_agent1, optimizer=None, eligibility_traces=False)
+        net0.load(checkpoint_path=model_agent0, optimizer=None, eligibility_traces=False)
+        net1.load(checkpoint_path=model_agent1, optimizer=None, eligibility_traces=False)
 
-    agents = {WHITE: TDAgent(WHITE, net=net1), BLACK: TDAgent(BLACK, net=net0)}
+        agents = {WHITE: TDAgent(WHITE, net=net1), BLACK: TDAgent(BLACK, net=net0)}
 
-    evaluate_agents(agents, env, n_episodes)
+        evaluate_agents(agents, env, n_episodes)
 
 
 # ===================================== GNUBG PARAMETERS =====================================
@@ -120,18 +131,18 @@ def args_gnubg(args):
     port = args.port
     difficulty = args.difficulty
 
-    assert os.path.exists(model_agent0), print("The path {} doesn't exists".format(model_agent0))
+    if path_exists(model_agent0):
+        # assert os.path.exists(model_agent0), print("The path {} doesn't exists".format(model_agent0))
+        if model_type == 'nn':
+            net0 = TDGammon(hidden_units=hidden_units_agent0, lr=0.1, lamda=None, init_weights=False)
+        else:
+            net0 = TDGammonCNN(lr=0.0001)
 
-    if model_type == 'nn':
-        net0 = TDGammon(hidden_units=hidden_units_agent0, lr=0.1, lamda=None, init_weights=False)
-    else:
-        net0 = TDGammonCNN(lr=0.0001)
+        net0.load(checkpoint_path=model_agent0, optimizer=None, eligibility_traces=False)
 
-    net0.load(checkpoint_path=model_agent0, optimizer=None, eligibility_traces=False)
-
-    gnubg_interface = GnubgInterface(host=host, port=port)
-    gnubg_env = GnubgEnv(gnubg_interface, difficulty=difficulty, model_type=model_type)
-    evaluate_vs_gnubg(agent=TDAgentGNU(WHITE, net=net0, gnubg_interface=gnubg_interface), env=gnubg_env, n_episodes=n_episodes)
+        gnubg_interface = GnubgInterface(host=host, port=port)
+        gnubg_env = GnubgEnv(gnubg_interface, difficulty=difficulty, model_type=model_type)
+        evaluate_vs_gnubg(agent=TDAgentGNU(WHITE, net=net0, gnubg_interface=gnubg_interface), env=gnubg_env, n_episodes=n_episodes)
 
 
 # ===================================== PLOT PARAMETERS ======================================
@@ -154,60 +165,61 @@ def args_plot(args, parser):
     difficulties = args.difficulty.split(',')
     model_type = args.type
 
-    assert os.path.exists(src), print("The path {} doesn't exists".format(src))
+    if path_exists(src):
+        # assert os.path.exists(src), print("The path {} doesn't exists".format(src))
 
-    for d in difficulties:
-        if d not in ['beginner', 'intermediate', 'advanced', 'world_class']:
-            parser.error("--difficulty should be (one or more of) 'beginner','intermediate', 'advanced' ,'world_class'")
+        for d in difficulties:
+            if d not in ['beginner', 'intermediate', 'advanced', 'world_class']:
+                parser.error("--difficulty should be (one or more of) 'beginner','intermediate', 'advanced' ,'world_class'")
 
-    dst = args.dst
+        dst = args.dst
 
-    if 'gnubg' in opponents and (not host or not port):
-        parser.error("--host and --port are required when 'gnubg' is specified in --opponent")
+        if 'gnubg' in opponents and (not host or not port):
+            parser.error("--host and --port are required when 'gnubg' is specified in --opponent")
 
-    for root, dirs, files in os.walk(src):
-        global_step = 0
-        files = sorted(files)
+        for root, dirs, files in os.walk(src):
+            global_step = 0
+            files = sorted(files)
 
-        writer = SummaryWriter(dst)
+            writer = SummaryWriter(dst)
 
-        for file in files:
-            if ".tar" in file:
-                print("\nLoad {}".format(os.path.join(root, file)))
+            for file in files:
+                if ".tar" in file:
+                    print("\nLoad {}".format(os.path.join(root, file)))
 
-                if model_type == 'nn':
-                    net = TDGammon(hidden_units=hidden_units, lr=0.1, lamda=None, init_weights=False)
-                    env = gym.make('gym_backgammon:backgammon-v0')
-                else:
-                    net = TDGammonCNN(lr=0.0001)
-                    env = gym.make('gym_backgammon:backgammon-pixel-v0')
+                    if model_type == 'nn':
+                        net = TDGammon(hidden_units=hidden_units, lr=0.1, lamda=None, init_weights=False)
+                        env = gym.make('gym_backgammon:backgammon-v0')
+                    else:
+                        net = TDGammonCNN(lr=0.0001)
+                        env = gym.make('gym_backgammon:backgammon-pixel-v0')
 
-                net.load(checkpoint_path=os.path.join(root, file), optimizer=None, eligibility_traces=False)
+                    net.load(checkpoint_path=os.path.join(root, file), optimizer=None, eligibility_traces=False)
 
-                if 'gnubg' in opponents:
-                    tag_scalar_dict = {}
+                    if 'gnubg' in opponents:
+                        tag_scalar_dict = {}
 
-                    gnubg_interface = GnubgInterface(host=host, port=port)
+                        gnubg_interface = GnubgInterface(host=host, port=port)
 
-                    for difficulty in difficulties:
-                        gnubg_env = GnubgEnv(gnubg_interface, difficulty=difficulty, model_type=model_type)
-                        wins = evaluate_vs_gnubg(agent=TDAgentGNU(WHITE, net=net, gnubg_interface=gnubg_interface), env=gnubg_env, n_episodes=n_episodes)
-                        tag_scalar_dict[difficulty] = wins[WHITE]
+                        for difficulty in difficulties:
+                            gnubg_env = GnubgEnv(gnubg_interface, difficulty=difficulty, model_type=model_type)
+                            wins = evaluate_vs_gnubg(agent=TDAgentGNU(WHITE, net=net, gnubg_interface=gnubg_interface), env=gnubg_env, n_episodes=n_episodes)
+                            tag_scalar_dict[difficulty] = wins[WHITE]
 
-                    writer.add_scalars('wins_vs_gnubg/', tag_scalar_dict, global_step)
+                        writer.add_scalars('wins_vs_gnubg/', tag_scalar_dict, global_step)
 
-                    with open(root + '/results.txt', 'a') as f:
-                        print("{};".format(file) + str(tag_scalar_dict), file=f)
+                        with open(root + '/results.txt', 'a') as f:
+                            print("{};".format(file) + str(tag_scalar_dict), file=f)
 
-                if 'random' in opponents:
-                    tag_scalar_dict = {}
-                    agents = {WHITE: TDAgent(WHITE, net=net), BLACK: RandomAgent(BLACK)}
-                    wins = evaluate_agents(agents, env, n_episodes)
+                    if 'random' in opponents:
+                        tag_scalar_dict = {}
+                        agents = {WHITE: TDAgent(WHITE, net=net), BLACK: RandomAgent(BLACK)}
+                        wins = evaluate_agents(agents, env, n_episodes)
 
-                    tag_scalar_dict['random'] = wins[WHITE]
+                        tag_scalar_dict['random'] = wins[WHITE]
 
-                    writer.add_scalars('wins_vs_random/', tag_scalar_dict, global_step)
+                        writer.add_scalars('wins_vs_random/', tag_scalar_dict, global_step)
 
-                global_step += 1
+                    global_step += 1
 
-                writer.close()
+                    writer.close()
